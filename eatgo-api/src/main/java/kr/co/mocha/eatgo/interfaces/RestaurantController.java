0b953a0941a862fd7d91ac5d3416e3b2f8cda64b
sedin <leesejinn@naver.com>
2020-03-07 19:54:49 +0900

    Make : RestaurantRepository
    RestaurantRepository 생성 후 중복 제거

--- eatgo-api/src/main/java/kr/co/mocha/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/mocha/eatgo/interfaces/RestaurantController.java
@@ -1,6 +1,7 @@
 package kr.co.mocha.eatgo.interfaces;
 
 import kr.co.mocha.eatgo.domain.Restaurant;
+import kr.co.mocha.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,25 +12,18 @@ import java.util.List;
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository restaurantRepository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
-
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = restaurantRepository.findAll();
 
         return restaurants;
     }
 
     @GetMapping("restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
-
-        Restaurant restaurant = restaurants.stream().filter(r -> r.getId().equals(id)).findFirst().orElse(null);
+        Restaurant restaurant = restaurantRepository.findById(id);
 
         return restaurant;
     }
