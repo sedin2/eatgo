cb8c58375a6119f19fe9c6befb8204e6c99c9fa5
sedin <leesejinn@naver.com>
2020-03-07 16:53:58 +0900

    Init : Spring boot project
    localhost:8080 print hello world

---
+++ eatgo-api/src/main/java/kr/co/mocha/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,13 @@
+package kr.co.mocha.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World@@";
+    }
+}
