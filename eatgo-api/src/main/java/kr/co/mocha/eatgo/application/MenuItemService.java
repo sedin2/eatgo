1dcd3941125474978bbcb08ca1eb332acc047bad
sedin <leesejinn@naver.com>
2020-03-19 23:59:32 +0900

    Add : MenuItemControllerTest code
    MenuItemControllerTest에 bulkUpdate() 테스트 코드 작성

---
+++ eatgo-api/src/main/java/kr/co/mocha/eatgo/application/MenuItemService.java
@@ -0,0 +1,14 @@
+package kr.co.mocha.eatgo.application;
+
+import kr.co.mocha.eatgo.domain.MenuItem;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems) {
+
+    }
+}
--- eatgo-api/src/test/java/kr/co/mocha/eatgo/interfaces/MenuItemControllerTests.java
+++ eatgo-api/src/test/java/kr/co/mocha/eatgo/interfaces/MenuItemControllerTests.java
@@ -1,24 +1,30 @@
 package kr.co.mocha.eatgo.interfaces;
 
+import kr.co.mocha.eatgo.application.MenuItemService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(RestaurantController.class)
+@WebMvcTest(MenuItemController.class)
 class MenuItemControllerTests {
 
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    private MenuItemService menuItemService;
 
     @Test
     public void bulkUpdate() throws Exception {
@@ -27,6 +33,7 @@ class MenuItemControllerTests {
                 .content("[]"))
                 .andExpect(status().isOk());
 
+        verify(menuItemService).bulkUpdate(eq(1L), any());
     }
 
 }
