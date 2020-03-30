b1e96ba08e493736246a122b1b082c20c213c3c9
sedin <leesejinn@naver.com>
2020-03-30 23:04:38 +0900

    Implement : ReviewController 구현
    ReviewController만들고 ReviewService를 이용하여 addReview구현
    ReviewControllerTests 작성

---
+++ eatgo-api/src/main/java/kr/co/mocha/eatgo/application/ReviewService.java
@@ -0,0 +1,12 @@
+package kr.co.mocha.eatgo.application;
+
+import kr.co.mocha.eatgo.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    public void addReview(Review review) {
+
+    }
+}
